7c93e5bf49fe8c4dde7c8e71db0f9c948ec854b2
rakuda-daraku <rakugaki.oukoku@gmail.com>
2018-07-18 22:52:03 +0900

    first commit

---
+++ raster/01/js/app.js
@@ -0,0 +1,22 @@
+let map = new mapboxgl.Map({
+  container: 'map',
+  center: [141.3564, 43.0611],
+  zoom: 16,
+  style: {
+    "version": 8,
+    "sources": {
+      "OSM": {
+        "type": "raster",
+        "tiles": ['http://a.tile.openstreetmap.org/{z}/{x}/{y}.png'],
+        "tileSize": 256
+      }
+    },
+    "layers": [{
+      "id": "OSM",
+      "type": "raster",
+      "source": "OSM",
+      "minzoom": 0,
+      "maxzoom": 22
+    }]
+  }
+});
